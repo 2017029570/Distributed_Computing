7cda69bfb4c9a7124f317f75b927933a6bdb5d16
2017029570 <48683310+2017029570@users.noreply.github.com>
2020-12-04 21:48:49 +0900

    Add files via upload

---
+++ fabric_project_interface/chaincode/java/src/main/java/org/hyperledger/fabric/samples/fabcar/Item.java
@@ -0,0 +1,71 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.hyperledger.fabric.samples.fabcar;
+
+import java.util.Objects;
+
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+
+import com.owlike.genson.annotation.JsonProperty;
+
+
+@DataType()
+public final class Item {
+
+    @Property()
+    private final String name;
+
+    @Property()
+    private final String owner;
+
+    @Property()
+    private final int status;
+
+    @Property()
+    private final String id;
+
+    @Property()
+    private final int price;
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public Item(@JsonProperty("key") final String id, @JsonProperty("name") final String make, @JsonProperty("owner") final String owner, @JsonProperty("status") final int status, @JsonProperty("price") final int price) {
+        this.id = id;
+        this.name = name;
+        this.owner = owner;
+        this.status = status;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+
+        Item other = (Item) obj;
+
+        return Objects.deepEquals(new String[] {getName(), getOwner()},
+                new String[] {other.getName(), other.getOwner()});
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getOwner());
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "@" + Integer.toHexString(hashCode()) + " [make=" + make + ", model="
+                + model + ", color=" + color + ", owner=" + owner + "]";
+    }
+}
